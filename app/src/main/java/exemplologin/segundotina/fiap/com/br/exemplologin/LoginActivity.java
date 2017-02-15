8d03a1128d91797e84c496376411d5f58bf32bb4
Fernando Mazola <fernando_mazola@hotmail.com>
2017-02-15 10:34:55 -0200

    first commit

---
+++ app/src/main/java/exemplologin/segundotina/fiap/com/br/exemplologin/LoginActivity.java
@@ -0,0 +1,13 @@
+package exemplologin.segundotina.fiap.com.br.exemplologin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
