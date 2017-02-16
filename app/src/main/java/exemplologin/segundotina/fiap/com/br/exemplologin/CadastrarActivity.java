578593f4d1ebaf2d36a1d873959152f94c2ab29a
logonrm <logonrm@fiap.com.br>
2017-02-16 11:43:35 -0200

    Adicionado componentes via Design

--- app/src/main/java/exemplologin/segundotina/fiap/com/br/exemplologin/CadastrarActivity.java
+++ app/src/main/java/exemplologin/segundotina/fiap/com/br/exemplologin/CadastrarActivity.java
@@ -2,6 +2,8 @@ package exemplologin.segundotina.fiap.com.br.exemplologin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class CadastrarActivity extends AppCompatActivity {
 
@@ -9,6 +11,14 @@ public class CadastrarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar);
+
+        RadioButton rdb = (RadioButton)findViewById(R.id.rdbMasculino);
+        if(rdb.isChecked()){
+            Toast.makeText(this, "Selevcionado", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Você não selecionou", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 }
